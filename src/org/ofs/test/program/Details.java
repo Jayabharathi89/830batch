b15d83fbfa062cb33dea6520a7c8cdd7c16d5c16
jayabharathi89 <jayabharathi.bharathi26@gmail.com>
2020-01-28 12:09:32 +0530

    New code added

---
+++ src/org/ofs/test/program/Details.java
@@ -0,0 +1,13 @@
+package org.ofs.test.program;
+
+public class Details {
+	public void getName() {
+	System.out.println("my name is jaya");
+}
+	public static void main(String[] args) {
+		Details d = new Details();
+		d.getName();
+		
+	}
+
+}
